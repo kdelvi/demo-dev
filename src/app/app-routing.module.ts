5b66afe96abcaf16a464f26553a2226852ad507c
kdelvi <kdelvi1@gmail.com>
2019-09-05 07:06:02 +0000

    router added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './core/home/home.component';
+import { TimelineComponent } from './core/timeline/timeline.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: HomeComponent
+  },
+  {
+    path: 'pages',
+    component: TimelineComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
